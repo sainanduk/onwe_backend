b20053f3b4248431f33a1b3532c72288d26f1239
sainandu <131136038+sainanduk@users.noreply.github.com>
2024-06-28 21:16:57 +0530

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,58 +1,21 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../Config/database');
+const { sequelize } = require('../config/database');
 
 const Users = sequelize.define('users', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  fullname: {
-    type: DataTypes.STRING,
-  },
-  avatar: {
-    type: DataTypes.STRING,
-  },
-  bio: {
-    type: DataTypes.STRING,
-  },
-  department: {
-    type: DataTypes.STRING,
-  },
-  role: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  coverimg: {
-    type: DataTypes.STRING,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  refreshToken: {
-    type: DataTypes.STRING,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-}, {
-  tableName: 'users',
+  id: { type: DataTypes.STRING, primaryKey: true },
+  username: { type: DataTypes.STRING, unique: true },
+  email: DataTypes.STRING,
+  fullname: DataTypes.STRING,
+  avatar: DataTypes.STRING,
+  bio: DataTypes.STRING,
+  department: DataTypes.STRING,
+  role: DataTypes.BOOLEAN,
+  coverimg: DataTypes.STRING,
+  password: DataTypes.STRING,
+  refreshToken: DataTypes.STRING,
+  clubs: {type: DataTypes.ARRAY(DataTypes.INTEGER),defaultValue: []},
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE,
 });
 
 module.exports = Users;
