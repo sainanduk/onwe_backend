1e6de7205e33d73440fc6e81051fab12a792a171
sainandu <131136038+sainanduk@users.noreply.github.com>
2024-06-28 21:15:48 +0530

    Update Comments.js

--- models/Comments.js
+++ models/Comments.js
@@ -1,20 +1,13 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../Config/database');
+const sequelize = require('../config/database'); // adjust the path as necessary
 
 const Comments = sequelize.define('comments', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   postId: { type: DataTypes.INTEGER, references: { model: 'posts', key: 'id' } },
-  userId: { type: DataTypes.INTEGER, references: { model: 'users', key: 'id' } },
-  content: DataTypes.STRING,
-  replies: DataTypes.ARRAY(DataTypes.INTEGER),
-  createdAt: DataTypes.DATE,
-  updatedAt: DataTypes.DATE,
-}, {
-  tableName: 'comments',
+  userId: { type: DataTypes.STRING, references: { model: 'users', key: 'id' } },
+  content: { type: DataTypes.STRING, allowNull: false },
+  parentId: { type: DataTypes.INTEGER, allowNull: true, defaultValue: null, references: { model: 'comments', key: 'id' } },
+  createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 });
 
 module.exports = Comments;
